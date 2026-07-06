c5a93765e49b9fb6f58536bf03ee2d618c9377fa
Vladimir Marko <vladimirmarko1988@gmail.com>
2026-07-06 18:19:02 +0000

    Clarify Todo token codec derivation

--- apps/todo/src/env.ts
+++ apps/todo/src/env.ts
@@ -29,9 +29,21 @@ export interface TodoServiceConfig {
   port: number
 }
 
+const deriveIdentityTokenCodec = (env: TodoEnv): TodoIdentityTokenCodec => {
+  if (env.IDENTITY_TOKEN_CODEC !== undefined) {
+    return env.IDENTITY_TOKEN_CODEC
+  }
+
+  if (env.MEGIDDO_AUTH_PROFILE === 'local-dummy') {
+    return 'dummy'
+  }
+
+  return 'jwt-jws'
+}
+
 export const createTodoServiceConfig = (env: TodoEnv): TodoServiceConfig => ({
   databasePath: env.TODO_DATABASE_PATH,
-  identityTokenCodec: env.IDENTITY_TOKEN_CODEC ?? (env.MEGIDDO_AUTH_PROFILE === 'local-dummy' ? 'dummy' : 'jwt-jws'),
+  identityTokenCodec: deriveIdentityTokenCodec(env),
   identityTokenPublicKeyPemBase64: env.MEGIDDO_IDENTITY_TOKEN_PUBLIC_KEY_PEM_BASE64,
   port: env.PORT,
 })
